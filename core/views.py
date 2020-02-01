8da4fef6a239f3c228edaaa2975dc9e7f8083030
Raza Poonja <razapoonja@gmail.com>
2020-02-02 02:08:00 +0500

    tinkered around with different views

--- core/views.py
+++ core/views.py
@@ -1,3 +1,33 @@
+from django.http import JsonResponse
 from django.shortcuts import render
 
-# Create your views here.
+from rest_framework import mixins, generics
+from rest_framework.views import APIView
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+
+from .serializers import PostSerializer
+from .models import Post
+
+class PostView(mixins.ListModelMixin, mixins.CreateModelMixin, generics.GenericAPIView):
+    serializer_class = PostSerializer
+    queryset = Post.objects.all()
+
+    # permission_classes = (IsAuthenticated, )
+
+    def get(self, request, *args, **kwargs):
+        return self.list(self, request, *args, **kwargs)
+
+    def post(self, request, *args, **kwargs):
+        return self.create(self, request, *args, **kwargs)
+
+
+    # Function based view
+    # def post(self, request, *args, **kwargs):
+    #     serializer = PostSerializer(data=request.data)
+
+    #     if serializer.is_valid():
+    #         serializer.save()
+    #         return Response(serializer.data)
+
+    #     return Response(serializer.errors)
--- django_api/settings.py
+++ django_api/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'rest_framework.authtoken',
+    'core',
 ]
 
 MIDDLEWARE = [
@@ -118,3 +121,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication'
+    ]
+}
